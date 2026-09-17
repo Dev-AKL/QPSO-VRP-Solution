8ef8a7fc95020e14df172b34c54226eb4316a244
Gourav.. <thgourav01@gmail.com>
2026-09-17 21:47:13 +0530

    Change graph loading method from place to point

--- core/api.py
+++ core/api.py
@@ -49,7 +49,7 @@ def prepare_osm_graph(place_name: str):
         return GRAPH_CACHE[place_name]
 
     # Load unprojected graph (WGS84 EPSG:4326) for GeoJSON GPS coordinates
-    G_raw = ox.graph_from_place((22.5867, 88.4171),
+    G_raw = ox.graph_from_point((22.5867, 88.4171),
     dist=2000, network_type="drive", simplify=True)
     
     # Guarantee legal bidirectional driving paths exist between all node pairs
